6c932b0eb780437deac9a6dff10a093191dd1687
sonal <sonalsingh4102@gmail.com>
2022-12-25 15:55:45 +0530

    added links of projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,16 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, gotoUrl }) => {
   return (
-    <Col  sm={6} md={4}>
+    <Col sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
+
         <div className="proj-txtx">
-          <h4>{title}</h4>
+          {/* <h4 >{title}</h4> */}
+          <h4>
+            <a href={gotoUrl}>{title}</a>
+          </h4>
           <span>{description}</span>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,11 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
+import projImg7 from "../assets/img/project-img7.png";
+import projImg8 from "../assets/img/project-img8.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -12,22 +17,63 @@ export const Projects = () => {
   const projects = [
     {
       title: "Website",
-      // description: "Design & Development",
+      description: "College Project",
       imgUrl: projImg2,
+      gotoUrl:"https://www.linkedin.com/posts/sonal-singh-7899ab255_html-css-javascript-activity-7007994718009659392-qwIv?utm_source=share&utm_medium=member_desktop"
     },
     {
       title: "Website",
-      // description: "Design & Development",
+      description: "Company Assignment",
       imgUrl: projImg3,
+      gotoUrl:"https://patilkaki.sonalsingh14.repl.co/#home"
     },
     {
       title: "ToDo List",
-      // description: "Design & Development",
+      description: "React Project",
       imgUrl: projImg1,
+      gotoUrl:"https://github.com/sonal4102/todos-list.git"
     },
    
   ];
-
+  const projects2 = [
+    {
+      title: "Registration Form",
+      description: "Using React",
+      imgUrl: projImg4,
+      gotoUrl:"https://github.com/sonal4102/registrationFormreact.git"
+    },
+    {
+      title: "Day-Night Toggle",
+      description: "Using Advanced CSS",
+      imgUrl: projImg6,
+      gotoUrl:"https://www.linkedin.com/posts/sonal-singh-7899ab255_html-css-javascript-activity-7008005352478687232-Po67?utm_source=share&utm_medium=member_desktop"
+    },
+    {
+      title: "Circular Bar",
+      description: "Using HTML & CSS",
+      imgUrl: projImg7,
+      gotoUrl:"https://www.linkedin.com/posts/sonal-singh-7899ab255_html-css-javascript-activity-7008076824970764288-BVMO?utm_source=share&utm_medium=member_desktop"
+    },
+    
+   
+  ];
+  const projects3 = [
+    {
+      title: "Registration Form",
+      description: "Using HTML,CSS,JS",
+      imgUrl: projImg5,
+      gotoUrl:"https://www.linkedin.com/posts/sonal-singh-7899ab255_html-css-javascript-activity-7008002719370145792-KYgI?utm_source=share&utm_medium=member_desktop"
+    },
+    {
+      title: "Calculator",
+      description: "JS & CSS",
+      imgUrl: projImg8,
+      gotoUrl:"https://www.linkedin.com/posts/sonal-singh-7899ab255_project-html-css-activity-7002890405927145472-d5Pn?utm_source=share&utm_medium=member_desktop"
+    },
+   
+    
+   
+  ];
   return (
     <section className="project" id="project">
       <Container>
@@ -66,10 +112,32 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                                <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Row>
+                        {
+                          projects3.map((project, index) => {
+                            return (
+                                <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
